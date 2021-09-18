dd62d1f294a78576f5c65e9a6df13bfe3838b11f
Gordon Lim <honwei189@gmail.com>
2021-09-18 16:40:30 +0800

    Update "example" of using Flayer

--- examples/tests.php
+++ examples/tests.php
@@ -4,10 +4,17 @@ require __DIR__ . '/../vendor/autoload.php';
 honwei189\Flayer\Config::load();
 honwei189\Flayer\Data::$_user = "abc";
 
-$app = new honwei189\Flayer\Core;
-$crypto = $app->bind("honwei189\\crypto");
+$app    = new honwei189\Flayer\Core;
+$crypto = $app->bind("honwei189\\Flayer\\Crypto");
 
 echo $crypto->decrypt($crypto->encrypt("Test"));
+
+echo "------------------<br>";
+
+unset($app);
+$app = app("Flayer"); // Or use this method instead of $app = new honwei189\Flayer\Core;
+$app->bind("honwei189\\Flayer\\Crypto");
+echo app("Crypto")->decrypt(app("Crypto")->encrypt("Test"));
 
 echo "------------------<br>";
 
@@ -16,3 +23,4 @@ echo honwei189\Flayer\Crypto::d(honwei189\Flayer\Crypto::e("Test"));
 echo "------------------<br>";
 
 pre(honwei189\Flayer\Data::all());
+
